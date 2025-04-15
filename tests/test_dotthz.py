fd0c0fdd13913974176e97ef993d2358ef974bfb
hacknus <linus.stoeckli@unibe.ch>
2025-04-15 21:10:10 +0200

    fixed not writing to file and tests

--- tests/test_dotthz.py
+++ tests/test_dotthz.py
@@ -24,8 +24,8 @@ class TestDotthzFile(unittest.TestCase):
                 copied_dotthz_file.measurements = original_measurements
 
             # Load data from the new copy file
-            with DotthzFile(path) as copied_dotthz_file:
-
+            with DotthzFile(path) as original_dotthz_file, DotthzFile(copy_file_path) as copied_dotthz_file:
+                original_measurements = original_dotthz_file.measurements
                 # Compare the original and copied Dotthz structures
                 self.assertEqual(len(original_measurements), len(copied_dotthz_file.measurements))
 
@@ -68,6 +68,10 @@ class TestDotthzFile(unittest.TestCase):
         datasets = {
             "ds1": np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
         }
+
+        # create deep copy
+        original_datasets = {key: val for key, val in datasets.items()}
+
         meta_data = DotthzMetaData(
             user="Test User",
             email="test@example.com",
@@ -96,31 +100,30 @@ class TestDotthzFile(unittest.TestCase):
             # Compare original and loaded data
             self.assertEqual(len(measurements), len(loaded_file.measurements))
 
-            for group_name, measurement in file_to_write.measurements.items():
-                loaded_measurement = loaded_file.measurements[group_name]
-                self.assertIsNotNone(loaded_measurement)
+            loaded_measurement = loaded_file.measurements["Measurement 1"]
+            self.assertIsNotNone(loaded_measurement)
 
-                # Compare metadata fields
-                self.assertEqual(measurement.meta_data.user, loaded_measurement.meta_data.user)
-                self.assertEqual(measurement.meta_data.email, loaded_measurement.meta_data.email)
-                self.assertEqual(measurement.meta_data.orcid, loaded_measurement.meta_data.orcid)
-                self.assertEqual(measurement.meta_data.institution, loaded_measurement.meta_data.institution)
-                self.assertEqual(measurement.meta_data.description, loaded_measurement.meta_data.description)
-                self.assertEqual(measurement.meta_data.version, loaded_measurement.meta_data.version)
-                self.assertEqual(measurement.meta_data.mode, loaded_measurement.meta_data.mode)
-                self.assertEqual(measurement.meta_data.instrument, loaded_measurement.meta_data.instrument)
-                self.assertEqual(measurement.meta_data.time, loaded_measurement.meta_data.time)
-                self.assertEqual(measurement.meta_data.date, loaded_measurement.meta_data.date)
+            # Compare metadata fields
+            self.assertEqual(meta_data.user, loaded_measurement.meta_data.user)
+            self.assertEqual(meta_data.email, loaded_measurement.meta_data.email)
+            self.assertEqual(meta_data.orcid, loaded_measurement.meta_data.orcid)
+            self.assertEqual(meta_data.institution, loaded_measurement.meta_data.institution)
+            self.assertEqual(meta_data.description, loaded_measurement.meta_data.description)
+            self.assertEqual(meta_data.version, loaded_measurement.meta_data.version)
+            self.assertEqual(meta_data.mode, loaded_measurement.meta_data.mode)
+            self.assertEqual(meta_data.instrument, loaded_measurement.meta_data.instrument)
+            self.assertEqual(meta_data.time, loaded_measurement.meta_data.time)
+            self.assertEqual(meta_data.date, loaded_measurement.meta_data.date)
 
-                # Compare metadata's key-value pairs
-                self.assertEqual(measurement.meta_data.md, loaded_measurement.meta_data.md)
+            # Compare metadata's key-value pairs
+            self.assertEqual(meta_data.md, loaded_measurement.meta_data.md)
 
-                # Compare datasets
-                self.assertEqual(len(measurement.datasets), len(loaded_measurement.datasets))
-                for dataset_name, dataset in measurement.datasets.items():
-                    loaded_dataset = loaded_measurement.datasets.get(dataset_name)
-                    self.assertIsNotNone(loaded_dataset)
-                    np.testing.assert_array_equal(dataset, loaded_dataset)
+            # Compare datasets
+            self.assertEqual(len(original_datasets), len(loaded_measurement.datasets))
+            for dataset_name, dataset in original_datasets.items():
+                loaded_dataset = loaded_measurement.datasets.get(dataset_name)
+                self.assertIsNotNone(loaded_dataset)
+                np.testing.assert_array_equal(dataset, loaded_dataset)
 
         # Clean up temporary file
         os.remove(path)
@@ -133,6 +136,10 @@ class TestDotthzFile(unittest.TestCase):
         datasets = {
             "ds1": np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
         }
+
+        # create deep copy
+        original_datasets = {key: val for key, val in datasets.items()}
+
         meta_data = DotthzMetaData(
             user="Test User",
             email="test@example.com",
@@ -160,6 +167,74 @@ class TestDotthzFile(unittest.TestCase):
             # Compare original and loaded data
             self.assertEqual(len(measurements), len(loaded_file.measurements))
 
+            loaded_measurement = loaded_file["Measurement 1"]
+            self.assertIsNotNone(loaded_measurement)
+
+            # Compare metadata fields
+            self.assertEqual(meta_data.user, loaded_measurement.meta_data.user)
+            self.assertEqual(meta_data.email, loaded_measurement.meta_data.email)
+            self.assertEqual(meta_data.orcid, loaded_measurement.meta_data.orcid)
+            self.assertEqual(meta_data.institution, loaded_measurement.meta_data.institution)
+            self.assertEqual(meta_data.description, loaded_measurement.meta_data.description)
+            self.assertEqual(meta_data.version, loaded_measurement.meta_data.version)
+            self.assertEqual(meta_data.mode, loaded_measurement.meta_data.mode)
+            self.assertEqual(meta_data.instrument, loaded_measurement.meta_data.instrument)
+            self.assertEqual(meta_data.time, loaded_measurement.meta_data.time)
+            self.assertEqual(meta_data.date, loaded_measurement.meta_data.date)
+
+            # Compare metadata's key-value pairs
+            self.assertEqual(meta_data.md, loaded_measurement.meta_data.md)
+
+            # Compare datasets
+            self.assertEqual(len(original_datasets), len(loaded_measurement.datasets))
+            for dataset_name, dataset in original_datasets.items():
+                loaded_dataset = loaded_measurement.datasets[dataset_name]
+                self.assertIsNotNone(loaded_dataset)
+                np.testing.assert_array_equal(dataset, loaded_dataset)
+
+        # Clean up temporary file
+        os.remove(path)
+
+    def test_dotthz_extend_existing_dataset(self):
+        with NamedTemporaryFile(delete=False) as temp_file:
+            path = temp_file.name
+
+        # Initialize test data for Dotthz
+        datasets = {
+            "ds1": np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32)
+        }
+        meta_data = DotthzMetaData(
+            user="Test User",
+            email="test@example.com",
+            orcid="0000-0001-2345-6789",
+            institution="Test Institute",
+            description="Test description",
+            md={"md1": "Thickness (mm)"},
+            version="1.00",
+            mode="Test mode",
+            instrument="Test instrument",
+            time="12:34:56",
+            date="2024-11-08"
+        )
+
+        measurements = {
+            "Measurement 1": DotthzMeasurement(datasets=datasets, meta_data=meta_data)
+        }
+
+        with DotthzFile(path, "w") as file_to_write:
+
+            for name, measurement in measurements.items():
+                file_to_write.measurements[name] = measurement
+            file_to_write.measurements["Measurement 1"].datasets["ds1"][0, 0] = 0.0
+
+        # with DotthzFile(path, "r+") as file_to_extend:
+        #     file_to_extend.measurements["Measurement 1"].datasets["ds1"][0, 0] = 0.0
+
+        # Load from the temporary file
+        with DotthzFile(path) as loaded_file:
+            # Compare original and loaded data
+            self.assertEqual(len(measurements), len(loaded_file.measurements))
+
             for group_name, measurement in file_to_write.measurements.items():
                 loaded_measurement = loaded_file[group_name]
                 self.assertIsNotNone(loaded_measurement)
@@ -181,10 +256,11 @@ class TestDotthzFile(unittest.TestCase):
 
                 # Compare datasets
                 self.assertEqual(len(measurement.datasets), len(loaded_measurement.datasets))
+
                 for dataset_name, dataset in measurement.datasets.items():
                     loaded_dataset = loaded_measurement.datasets[dataset_name]
                     self.assertIsNotNone(loaded_dataset)
-                    np.testing.assert_array_equal(dataset, loaded_dataset)
+                    np.testing.assert_array_equal(loaded_dataset, np.array([[0.0, 2.0], [3.0, 4.0]], dtype=np.float32))
 
         # Clean up temporary file
         os.remove(path)
@@ -212,7 +288,7 @@ class TestDotthzFile(unittest.TestCase):
         )
 
         measurements = {
-            "Measurement 1": DotthzMeasurement(datasets=datasets, meta_data=meta_data)
+            "Measurement 1": DotthzMeasurement()
         }
 
         with DotthzFile(path, "w") as file_to_write:
@@ -220,7 +296,7 @@ class TestDotthzFile(unittest.TestCase):
                 file_to_write.measurements[name] = measurement
 
         with DotthzFile(path, "r+") as file_to_extend:
-            file_to_extend.measurements["Measurement 1"].datasets["ds1"][0, 0] = 0.0
+            file_to_extend.measurements["Measurement 1"] = DotthzMeasurement()
 
         # Load from the temporary file
         with DotthzFile(path) as loaded_file:
